54362561f8b2ba417c6e327a07cb56871c3134bc
Cristovao Cordeiro <cristovao.cordeiro@cern.ch>
2016-11-10 17:54:38 +0100

    missing fi

--- magnum/drivers/common/templates/swarm/fragments/enable-services.sh
+++ magnum/drivers/common/templates/swarm/fragments/enable-services.sh
@@ -6,6 +6,7 @@ for service in $NODE_SERVICES; do
     if [ "$service" = "swarm-cadvisor" ] && \\
       [ "$(echo $MONITORING_ENABLED | tr '[:upper:]' '[:lower:]')" = "false" ]; then
         continue
+    fi
     echo "activating service $service"
     systemctl enable $service
     systemctl --no-block start $service
