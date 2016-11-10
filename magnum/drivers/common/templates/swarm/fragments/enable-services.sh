cfd25126b0b8b2b785800aa68df8fa3e225ac71b
Cristovao Cordeiro <cristovao.cordeiro@cern.ch>
2016-11-10 17:41:05 +0100

    conditional enable cadvisor

--- magnum/drivers/common/templates/swarm/fragments/enable-services.sh
+++ magnum/drivers/common/templates/swarm/fragments/enable-services.sh
@@ -3,6 +3,9 @@
 echo "starting services"
 systemctl daemon-reload
 for service in $NODE_SERVICES; do
+    if [ "$service" = "swarm-cadvisor" ] && \\
+      [ "$(echo $MONITORING_ENABLED | tr '[:upper:]' '[:lower:]')" = "false" ]; then
+        continue
     echo "activating service $service"
     systemctl enable $service
     systemctl --no-block start $service
