e872ba4d8fbbc5ade1d9d679e1761e0be9fd10fc
Cristovao Cordeiro <cristovao.cordeiro@cern.ch>
2016-11-18 13:37:47 +0100

    fix mounted volume permissions

--- magnum/drivers/common/templates/fragments/prometheus.sh
+++ magnum/drivers/common/templates/fragments/prometheus.sh
@@ -7,23 +7,27 @@ fi
 # Create a shared directory on every node, with the conf file
 PROMETHEUS_SHARE="/opt/prometheus"
 mkdir -p $PROMETHEUS_SHARE
-echo "PROM_CONF_DIR=$PROMETHEUS_SHARE" > /etc/sysconfig/prometheus-conf-setup
+echo "PROM_CONF_DIR_HOST=$PROMETHEUS_SHARE" > /etc/sysconfig/prometheus-conf-setup
+
+PROMETHEUS_SHARE_MOUNT="/prometheus-data"
+echo "PROM_CONF_DIR_CONTAINER=$PROMETHEUS_SHARE_MOUNT" >> /etc/sysconfig/prometheus-conf-setup
 
 PROMETHEUS_CONF=$PROMETHEUS_SHARE"/prometheus.yml"
 SERVICE_DISCOVERY_FILE=$PROMETHEUS_SHARE"/service_discovery.yml"
 touch $SERVICE_DISCOVERY_FILE
+SERVICE_DISCOVERY_FILE_MOUNTED=$PROMETHEUS_SHARE_MOUNT"/prometheus.yml"
 
 cat > $PROMETHEUS_CONF << CONF_EOF
 global:
   scrape_interval: 10s
   evaluation_interval: 15s
-  scrape_timeout: 20s
+  scrape_timeout: 10s
 
 scrape_configs:
   - job_name: 'magnum-prometheus'
     file_sd_configs:
       - files:
-        - $SERVICE_DISCOVERY_FILE
+        - $SERVICE_DISCOVERY_FILE_MOUNTED
 CONF_EOF
 
 # Populate an initial basic service discovery
--- magnum/drivers/common/templates/swarm/fragments/swarm-launch-prometheus.sh
+++ magnum/drivers/common/templates/swarm/fragments/swarm-launch-prometheus.sh
@@ -14,9 +14,10 @@ SERVER_KEY="/etc/docker/server.key"
 
 docker -H $API_IP_ADDRESS:2376 --tlsverify --tlscacert $CLUSTER_CA \
                           --tlskey $SERVER_KEY --tlscert $SERVER_CERTIFICATE \
-                          run -t -p 9090:9090 -v $PROM_CONF_DIR:/prometheus-data \
+                          run -t -p 9090:9090 \
+                          -v $PROM_CONF_DIR_HOST:$PROM_CONF_DIR_CONTAINER \
                           -v /prometheus --link cadvisor:cadvisor \
-                          --link node-exporter:node-exporter
+                          --link node-exporter:node-exporter \
                           --name prometheus prom/prometheus \
-                          -config.file=/prometheus-data/prometheus.yml \
+                          -config.file=$PROM_CONF_DIR_CONTAINER"/prometheus.yml" \
                           -storage.local.path=/prometheus
