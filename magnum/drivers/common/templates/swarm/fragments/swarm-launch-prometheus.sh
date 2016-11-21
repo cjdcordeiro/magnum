ace745ecefc2e7c63fe1f37050e2e0f567837d58
Cristovao Cordeiro <cristovao.cordeiro@cern.ch>
2016-11-21 15:19:18 +0100

    escapes

--- magnum/drivers/common/templates/swarm/fragments/swarm-launch-prometheus.sh
+++ magnum/drivers/common/templates/swarm/fragments/swarm-launch-prometheus.sh
@@ -1,37 +1,36 @@
 #!/bin/sh
 
-START_PROMETHEUS="/usr/local/bin/start-prometheus"
+START_PROMETHEUS='/usr/local/bin/start-prometheus'
 
-CONTENT='''
+cat >$START_PROMETHEUS <<EOF
 #!/bin/sh
 . /etc/sysconfig/heat-params
 . /etc/sysconfig/prometheus-conf-setup
 
 # Setup the API client for swarm
-CLUSTER_CA="/etc/docker/ca.crt"
-SERVER_CERTIFICATE="/etc/docker/server.crt"
-SERVER_KEY="/etc/docker/server.key"
-while [ `docker -H $API_IP_ADDRESS:2376 --tlsverify --tlscacert $CLUSTER_CA \
-                          --tlskey $SERVER_KEY --tlscert $SERVER_CERTIFICATE \
-                          info | grep "Nodes:" | awk '{print $2}'` -lt 1 ]
+CLUSTER_CA='/etc/docker/ca.crt'
+SERVER_CERTIFICATE='/etc/docker/server.crt'
+SERVER_KEY='/etc/docker/server.key'
+
+while [ `docker -H \$API_IP_ADDRESS:2376 --tlsverify --tlscacert \$CLUSTER_CA \\
+                          --tlskey \$SERVER_KEY --tlscert \$SERVER_CERTIFICATE \\
+                          info | grep 'Nodes:' | awk '{print \$2}'` -lt 1 ]
 do
-  echo "Waiting for available nodes..."
+  echo 'Waiting for available nodes...'
   sleep 5
 done
 
 # Following will fail if already exists
-docker -H $API_IP_ADDRESS:2376 --tlsverify --tlscacert $CLUSTER_CA \
-                          --tlskey $SERVER_KEY --tlscert $SERVER_CERTIFICATE \
-                          run -t -p 9090:9090 \
-                          -v $PROM_CONF_DIR_HOST:$PROM_CONF_DIR_CONTAINER:z \
-                          -v /prometheus --link cadvisor:cadvisor \
-                          --link node-exporter:node-exporter \
-                          --name prometheus prom/prometheus \
-                          -config.file=$PROM_CONF_DIR_CONTAINER"/prometheus.yml" \
+docker -H \$API_IP_ADDRESS:2376 --tlsverify --tlscacert \$CLUSTER_CA \\
+                          --tlskey \$SERVER_KEY --tlscert \$SERVER_CERTIFICATE \\
+                          run -t -p 9090:9090 \\
+                          -v \$PROM_CONF_DIR_HOST:\$PROM_CONF_DIR_CONTAINER:z \\
+                          -v /prometheus --link cadvisor:cadvisor \\
+                          --link node-exporter:node-exporter \\
+                          --name prometheus prom/prometheus \\
+                          -config.file=\$PROM_CONF_DIR_CONTAINER'/prometheus.yml' \\
                           -storage.local.path=/prometheus
-'''
-
-echo $CONTENT > $START_PROMETHEUS
+EOF
 
 chown root:root $START_PROMETHEUS
 chmod 755 $START_PROMETHEUS
