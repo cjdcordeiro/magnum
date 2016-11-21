746a605a781d0ff2d79a5fca4c1e30b6417da6cf
Cristovao Cordeiro <cristovao.cordeiro@cern.ch>
2016-11-21 14:02:56 +0100

    literal strings

--- magnum/drivers/common/templates/swarm/fragments/swarm-launch-prometheus.sh
+++ magnum/drivers/common/templates/swarm/fragments/swarm-launch-prometheus.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
+START_PROMETHEUS="/usr/local/bin/start-prometheus"
+
+CONTENT='''
+#!/bin/sh
 . /etc/sysconfig/heat-params
 . /etc/sysconfig/prometheus-conf-setup
 
-START_PROMETHEUS="/usr/local/bin/start-prometheus"
-cat >$START_PROMETHEUS << EOF
 # Setup the API client for swarm
 CLUSTER_CA="/etc/docker/ca.crt"
 SERVER_CERTIFICATE="/etc/docker/server.crt"
 SERVER_KEY="/etc/docker/server.key"
-
 while [ `docker -H $API_IP_ADDRESS:2376 --tlsverify --tlscacert $CLUSTER_CA \
                           --tlskey $SERVER_KEY --tlscert $SERVER_CERTIFICATE \
-                          info | grep "Nodes:" | awk -F' ' '{print $2}'` -lt 1 ]
+                          info | grep "Nodes:" | awk '{print $2}'` -lt 1 ]
 do
   echo "Waiting for available nodes..."
   sleep 5
@@ -28,7 +29,9 @@ docker -H $API_IP_ADDRESS:2376 --tlsverify --tlscacert $CLUSTER_CA \
                           --name prometheus prom/prometheus \
                           -config.file=$PROM_CONF_DIR_CONTAINER"/prometheus.yml" \
                           -storage.local.path=/prometheus
-EOF
+'''
+
+echo $CONTENT > $START_PROMETHEUS
 
 chown root:root $START_PROMETHEUS
 chmod 755 $START_PROMETHEUS
