1a3fc5a2d29d2ddd337cf96bffa3a84b03b34059
Cristovao Cordeiro <cristovao.cordeiro@cern.ch>
2016-11-22 09:53:38 +0100

    affinity to prometheus

--- magnum/drivers/common/templates/swarm/fragments/swarm-launch-prometheus-grafana.sh
+++ magnum/drivers/common/templates/swarm/fragments/swarm-launch-prometheus-grafana.sh
@@ -23,11 +23,10 @@ done
 # Following will fail if already exists
 docker -H \$API_IP_ADDRESS:2376 --tlsverify --tlscacert \$CLUSTER_CA \\
                           --tlskey \$SERVER_KEY --tlscert \$SERVER_CERTIFICATE \\
-                          run -t -p 9090:9090 \\
+                          run -t -d -p 9090:9090 \\
                           -v \$PROM_CONF_DIR_HOST:\$PROM_CONF_DIR_CONTAINER:z \\
                           -v /prometheus --link cadvisor:cadvisor \\
                           --link node-exporter:node-exporter \\
-                          -e constraint:node==\`hostname -s\` \\
                           --name prometheus prom/prometheus \\
                           -config.file=\$PROM_CONF_DIR_CONTAINER'/prometheus.yml' \\
                           -storage.local.path=/prometheus
@@ -35,7 +34,7 @@ docker -H \$API_IP_ADDRESS:2376 --tlsverify --tlscacert \$CLUSTER_CA \\
 docker -H \$API_IP_ADDRESS:2376 --tlsverify --tlscacert \$CLUSTER_CA \\
                           --tlskey \$SERVER_KEY --tlscert \$SERVER_CERTIFICATE \\
                           run -t -p 3000:3000 -v /var/lib/grafana:/var/lib/grafana \\
-                          -e constraint:node==\`hostname -s\` \\
+                          -e affinity:container==prometheus \\
                           -e "GF_SECURITY_ADMIN_PASSWORD=admin" \\
                           --name grafana grafana/grafana
 
@@ -45,7 +44,7 @@ docker -H \$API_IP_ADDRESS:2376 --tlsverify --tlscacert \$CLUSTER_CA \\
                           -v /etc/sysconfig:/etc/sysconfig \\
                           -v $PROM_SD_CRON:/etc/cron.d/
                           -v \$PROM_CONF_DIR_HOST:\$PROM_CONF_DIR_CONTAINER:z \\
-                          -e constraint:node==\`hostname -s\` \\
+                          -e affinity:container==prometheus \\
                           --name prometheus_auto_sd fedora
 EOF
 
