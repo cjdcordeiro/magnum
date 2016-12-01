38a517c8bece92f82766b9e318b461b8f44b654a
Cristovao Cordeiro <cristovao.cordeiro@cern.ch>
2016-12-01 16:28:56 +0100

    inspect promehteus and get node ip

--- magnum/drivers/common/templates/swarm/fragments/swarm-launch-prometheus-grafana.sh
+++ magnum/drivers/common/templates/swarm/fragments/swarm-launch-prometheus-grafana.sh
@@ -68,14 +68,14 @@ docker -H \$API_IP_ADDRESS:2376 --tlsverify --tlscacert \$CLUSTER_CA \\
 PROMETHEUS_IP=\`docker -H \$API_IP_ADDRESS:2376 --tlsverify --tlscacert \$CLUSTER_CA  \\
                           --tlskey \$SERVER_KEY --tlscert \$SERVER_CERTIFICATE \\
                           inspect prometheus | python -c \\
-                          "import sys, json; print json.load(sys.stdin)[0]['NetworkSettings']['IPAddress']"\`
+                          "import sys, json; print json.load(sys.stdin)[0]['Node']['IP']"\`
 
 curl --user admin:$GRAFANA_ADMIN_PASSWD -X POST \\
                           -H 'Content-Type: application/json;charset=UTF-8' \\
                           --data-binary \\
                           '{"name":"Swarm Monitoring","isDefault":true,
-                            "type":"prometheus","url":"http://\$PROMETHEUS_IP:9090",
-                            "access":"direct"}' "http://\$PROMETHEUS_IP:3000/api/datasources/"
+                            "type":"prometheus","url":"http://'\$PROMETHEUS_IP':9090",
+                            "access":"proxy"}' "http://\$PROMETHEUS_IP:3000/api/datasources/"
 EOF
 
 chown root:root $START_PROMETHEUS
